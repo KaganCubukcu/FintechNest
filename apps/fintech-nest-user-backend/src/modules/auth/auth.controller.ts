7dbdd60d930367f9c6d74aa09bff9802ca4f6156
Kağan Çubukçu <kagancancubukcu@gmail.com>
2024-07-30 12:17:06 +0300

    Implemented User Authentication with JWT

--- apps/fintech-nest-user-backend/src/modules/auth/auth.controller.ts
+++ apps/fintech-nest-user-backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './Dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('login')
+    async login(@Body() authDto: AuthDto): Promise<{ access_token: string }> {
+        return this.authService.login(authDto);
+    }
+}
